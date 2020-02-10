393ef8e1d42542d29809d7da221db3b74adc7c54
Guillaume CAESTECKER <17036@ecam.be>
2020-02-10 15:28:08 +0100

    db files

---
+++ Kitbox/ConnectMySQL/ConnectMySQL/DBUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Tutorial
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "mysql-kitbox2020.alwaysdata.net";
+            int port = 3306;
+            string database = "kitbox2020_ecam";
+            string username = "199703";
+            string password = "groupe2020";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
---
+++ Kitbox/ConnectMySQL/ConnectMySQL/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorial;
+using MySql.Data.MySqlClient;
+
+namespace ConnectMySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+                Console.WriteLine("Connection successful!");
+                SqlAdd("ClientMagasin","Age", "Nom", 20 ,"Myngheer", conn);
+                SqlSelect("Nom", "ClientMagasin", conn);
+                SqlDelete("ClientMagasin","NumClient",25,conn);
+                SqlSelect("Nom", "ClientMagasin", conn);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
+
+        }
+
+        static void SqlAdd(string table, string paramage,string paramname,int valueAge, string valueNom, MySqlConnection conn)
+        {
+
+            string query = "INSERT INTO "+ table+"("+paramname+","+paramage+") VALUE('"+valueNom+"',"+valueAge+")";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Rajouté");
+        }
+
+        static void SqlDelete(string table, string paramID, int valueID, MySqlConnection conn)
+        {
+            string query = "DELETE FROM " + table + " WHERE " + paramID + "=" + valueID;
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Supprimé");
+        }
+
+        static void SqlSelect(string paramID, string table, MySqlConnection conn)
+        {
+            string query = "SELECT " + paramID + " FROM " + table;
+            MySqlDataReader reader = new MySqlCommand(query, conn).ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader[paramID]);
+            }
+            reader.Close();
+           
+        }
+    }
+}
