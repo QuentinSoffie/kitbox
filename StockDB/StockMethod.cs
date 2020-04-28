b1e3a16068ca6d0044214866c43851d31a1b024b
Guillaume Caestecker <guillaume.caestecker@outlook.be>
2020-04-28 16:58:53 +0200

    Methode de gestion de stock

---
+++ StockDB/StockMethod.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace StockDB
+{
+	public class StockMethod
+	{
+
+
+		public static MySqlDataReader SearchComponent(string name, int height, int width, int depth, string color, MySqlConnection conn)
+		{
+			string query = "SELECT * FROM Piece" + " WHERE " + "REF ='" + name + "' AND hauteur ='" + height + "' AND largeur ='" + width + "' AND profondeur ='" + depth + "' AND Couleur ='" + color + "'" ;
+			MySqlDataReader reader = new MySqlCommand(query, conn).ExecuteReader();
+
+			return reader;
+		}
+
+		public static MySqlDataReader SearchOrderByName(string customerName, MySqlConnection conn)
+		{
+			string query = "SELECT * FROM Orders" + " WHERE " + "Customer ='" + customerName + "'";
+			MySqlDataReader reader = new MySqlCommand(query, conn).ExecuteReader();
+
+			return reader;
+		}
+
+		public static MySqlDataReader SearchOrderByNum(string numOrder, MySqlConnection conn)
+		{
+			string query = "SELECT * FROM Orders" + " WHERE " + "NumOrder ='" + numOrder + "'";
+			MySqlDataReader reader = new MySqlCommand(query, conn).ExecuteReader();
+
+			return reader;
+		}
+
+		public static void AddPart(string reference, string code, int dimension, int height, int width, int depth, string color, int inStock, int minStock, int priceCustomer, int qttyPart, int priceFour1, int delayFour1, int priceFour2, int delayFour2,  MySqlConnection conn)
+		{
+
+			string query = "INSERT INTO PIECE (Ref, Code, Dimension(cm), hauteur, profondeur, largeur,Couleur, Enstock, Stock minimum, Prix-Client, Nb-Pièces/casier, Prix-Fourn 1, Delai-Fourn 1, Prix-Fourn 2, Delai-Fourn 2) VALUE('" + reference + "','" + code + "','" + dimension + "','" + height + "','" + width + "','" + depth + "','" + color + "','" + inStock + "','" + minStock + "','" + priceCustomer + "','" + qttyPart + "','" + priceFour1 + "','" + delayFour1 + "','" + priceFour2 + "','" + delayFour2 + "')";
+			MySqlCommand cmd = new MySqlCommand(query, conn);
+			cmd.ExecuteNonQuery();
+		}
+
+		public static void Updateqtty(int code, int qtty, MySqlConnection conn)
+		{
+			conn.Open();
+			string query = "UPDATE Piece SET Enstock = '" + qtty + "' WHERE Code ='" + code +"'";
+			MySqlDataAdapter SDA = new MySqlDataAdapter(query, conn);
+			SDA.SelectCommand.ExecuteNonQuery();
+			conn.Close();
+		}
+	}
+}
