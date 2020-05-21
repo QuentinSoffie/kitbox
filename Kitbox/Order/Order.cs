875643ab803b22dfef75bca5ba3f7a7896ff3331
Guigxs <guigxs19@gmail.com>
2020-05-22 00:45:16 +0200

    Revert "Revert "Correction calcul pièces facture""
    This reverts commit e6c1fec0cbcd8ca75205cdec0dec1dfcd0114a3d.

--- Kitbox/Order/Order.cs
+++ Kitbox/Order/Order.cs
@@ -114,7 +114,7 @@ namespace Kitbox.Order
         private void PrepareOrder(Cupboard cupboard, List<List<string>> matrix)
         {
 
-            if (!(cupboard.CupboardAngle is null) && AddQuantityToOrder(cupboard.CupboardAngle.Code, matrix) == false)
+            if (!(cupboard.CupboardAngle is null))
             {
                 List<string> line = new List<string>() { cupboard.CupboardAngle.Code, "Cornieres", cupboard.CupboardAngle.DimensionsToString, cupboard.CupboardAngle.CountComponents().ToString() }; ;
                 matrix.Add(line);
@@ -122,27 +122,27 @@ namespace Kitbox.Order
 
             foreach (Box box in cupboard.ListeBoxes)
             {
-                if (!(box.Door is null) && AddQuantityToOrder(box.Door.Code, matrix) == false)
+                if (!(box.Door is null) && AddQuantityToOrder(box, box.Door.Code, matrix) == false)
                 {
                     List<string> line = new List<string>() { box.Door.Code, "Porte", box.Door.DimensionsToString, box.Door.CountComponents().ToString() }; ;
                     matrix.Add(line);
                 }
 
-                if (AddQuantityToOrder(box.Slider.Code, matrix) == false)
+                if (AddQuantityToOrder(box, box.Slider.Code, matrix) == false)
                 {
                     List<string> line = new List<string>() { box.Slider.Code, "Tasseau", box.Slider.DimensionsToString, box.Slider.CountComponents().ToString() }; ;
                     matrix.Add(line);
                 }
 
-                if (!(box.Cups is null) && AddQuantityToOrder(box.Cups.Code, matrix) == false)
+                if (!(box.Cups is null) && AddQuantityToOrder(box, box.Cups.Code, matrix) == false)
                 {
-                    List<string> line = new List<string>() { box.Cups.Code, "Tasseau", box.Cups.DimensionsToString, box.Cups.CountComponents().ToString() }; ;
+                    List<string> line = new List<string>() { box.Cups.Code, "Coupelles", box.Cups.DimensionsToString, box.Cups.CountComponents().ToString() }; ;
                     matrix.Add(line);
                 }
 
                 foreach (Traverses traverse in box.Traverses)
                 {
-                    if (AddQuantityToOrder(traverse.Code, matrix) == false)
+                    if (AddQuantityToOrder(box, traverse.Code, matrix) == false)
                     {
                         List<string> line = new List<string>() { traverse.Code, traverse.Type, traverse.DimensionsToString, traverse.CountComponents().ToString() }; ;
                         matrix.Add(line);
@@ -151,7 +151,7 @@ namespace Kitbox.Order
 
                 foreach (Kitbox.Components.Panel panel in box.Panels)
                 {
-                    if (AddQuantityToOrder(panel.Code, matrix) == false)
+                    if (AddQuantityToOrder(box, panel.Code, matrix) == false)
                     {
                         List<string> line = new List<string>() { panel.Code, "Panneau " + panel.Type, panel.DimensionsToString, panel.CountComponents().ToString() }; ;
                         matrix.Add(line);
@@ -163,15 +163,45 @@ namespace Kitbox.Order
         }
 
 
-        private bool AddQuantityToOrder(string code, List<List<string>> matrix) // Return index where order contains code (else -1)
+        private bool AddQuantityToOrder(Box box, string code, List<List<string>> matrix) // Return index where order contains code (else -1)
         {
           
             foreach (List<string> codeMatrix in matrix)
             {
+                //indice 1 = pièce
                 if (codeMatrix[0] == code) {
-                    codeMatrix[3] = (Int32.Parse(codeMatrix[3]) + 1).ToString();
+                    Console.WriteLine(String.Format("J'ajoute des {0}", codeMatrix[1]));
+
+                    if (codeMatrix[1] == "Porte")
+                    {
+                        codeMatrix[3] = (Int32.Parse(codeMatrix[3]) + box.Door.CountComponents()).ToString();
+                    }
+                    if (codeMatrix[1] == "Tasseau")
+                    {
+                        codeMatrix[3] = (Int32.Parse(codeMatrix[3]) + box.Slider.CountComponents()).ToString();
+                    }
+                    
+                    if (codeMatrix[1] == "Coupelles")
+                    {
+                        codeMatrix[3] = (Int32.Parse(codeMatrix[3]) + box.Cups.CountComponents()).ToString();
+                    }
+                    foreach (Kitbox.Components.Panel panel in box.Panels)
+                    {
+                        if (codeMatrix[1] == "Panneau " + panel.Type)
+                        {
+                            codeMatrix[3] = (Int32.Parse(codeMatrix[3]) + panel.CountComponents()).ToString();
+                        }
+                    }
+                    foreach (Traverses traverse in box.Traverses)
+                    {
+                        if (codeMatrix[1] == "Traverse " + traverse.Type)
+                        {
+                            codeMatrix[3] = (Int32.Parse(codeMatrix[3]) + traverse.CountComponents()).ToString();
+                        }
+                    }
                     return true;
                 }
+                
             }
             return false;
         }
